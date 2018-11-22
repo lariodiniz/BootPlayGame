dc3023f1ab48be2bff36c7b4f51ec0bf0420ce61
LarioDiniz <lario.diniz@palo.com.br>
2018-11-22 18:34:12 -0200

    Organizado a politica de Privacidade

--- bot_facebook/utils.py
+++ bot_facebook/utils.py
@@ -9,7 +9,7 @@ from django.conf import settings
 
 def post_facebook_message(fbid, recevied_message):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?' \
-                       'access_token={}'.format(settings.ACCESS_TOKEN)
+                       'access_token={}'.format(settings.FACEBOOK_BOT_TOKEN.lstrip().rstrip().replace(' ', ''))
     response_msg = json.dumps({"recipient": {"id": fbid},
                                "message": {"text": recevied_message}})
     status = requests.post(post_message_url,
