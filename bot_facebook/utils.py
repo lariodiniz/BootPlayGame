6ff8db7770e4e953d25315d1a96e6445b5dce9cd
LarioDiniz <lario.diniz@palo.com.br>
2018-11-22 19:28:29 -0200

    Update utils.py

--- bot_facebook/utils.py
+++ bot_facebook/utils.py
@@ -11,36 +11,44 @@ class BotFacebook:
 
     def __init__(self, id_usuario, mensagem_usuario):
 
+        self.id_usuario = id_usuario
+        self.mensagem_usuario = mensagem_usuario
+
         if mensagem_usuario == "/start":
-            self._verificaJogador(id_usuario)
+            self._verificaJogador()
 
+    def __CadastraJogador(self):
+        jogador = Jogadores()
+        jogador.nome = "Facebook"
+        jogador.id = int(self.id_usuario)
+        jogador.save()
 
-    def _verificaJogador(self, id_usuario):
+    def _verificaJogador(self):
 
-        if Jogadores.objects.filter(id=id_usuario).exists():
+        if Jogadores.objects.filter(id=self.id_usuario).exists():
             mensagem = "Você já é um jogador cadastrado."
-            self._send_message(id_usuario, mensagem)
+            self._send_message(self.id_usuario, mensagem)
         else:
             mensagem = "Você não é um jogador cadastrado. Estamos te cadastrando..."
             contador = 0
 
             while contador < 3:
-                self._send_message(id_usuario, mensagem)
+                self._send_message(self.id_usuario, mensagem)
                 self.__CadastraJogador()
-                if Jogadores.objects.filter(id=self.__telegram_id).exists():
+                if Jogadores.objects.filter(id=self.id_usuario).exists():
                     mensagem = "Você foi cadastrado com sucesso."
-                    self._send_message(id_usuario, mensagem)
+                    self._send_message(self.id_usuario, mensagem)
                     contador = 4
                 elif contador < 2:
                     mensagem = "Aconteceu um problema no cadastro, vamos tentar mais uma vez."
-                    self._send_message(id_usuario, mensagem)
+                    self._send_message(self.id_usuario, mensagem)
                 elif contador == 2:
                     mensagem = "Aconteceu um problema no cadastro, vamos tentar uma ultima vez."
-                    self._send_message(id_usuario, mensagem)
+                    self._send_message(self.id_usuario, mensagem)
                 contador += 1
             if contador == 3:
                 mensagem = "Não conseguimos te cadastrar, por favor, tente de novo mais tarde."
-                self._send_message(id_usuario, mensagem)
+                self._send_message(self.id_usuario, mensagem)
 
 
     def _send_message(fbid, recevied_message):
